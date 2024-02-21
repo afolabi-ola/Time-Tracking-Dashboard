100ad9c6c0587ebbe05a5895a47abbdac7f3050f
afolabi-ola <afolabiquadri28@gmail.com>
2024-02-21 13:03:31 +0100

    structuring JS and remodifying html css

---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const daily = document.querySelector(".Daily");
+const weekly = document.querySelector(".weekly");
+const monthly = document.querySelector(".Monthly");
+
+const workTitle = document.querySelector(".work-text");
+const playTitle = document.querySelector(".play-text");
+const studyTitle = document.querySelector(".study-text");
+const exerciseTitle = document.querySelector(".exercise-text");
+const socialTitle = document.querySelector(".social-text");
+const selfCareTitle = document.querySelector(".self-care-text");
+
+const workHrs = document.querySelector(".work-time");
+const playHrs = document.querySelector(".play-time");
+const studyHrs = document.querySelector(".study-time");
+const exerciseHrs = document.querySelector(".exercise-time");
+const socialHrs = document.querySelector(".social-time");
+const selfCareHrs = document.querySelector(".self-time");
+
+const workPreviousHrs = document.querySelector(".work-previous-hours");
+const playPreviousHrs = document.querySelector(".play-previous-hours");
+const studyPreviousHrs = document.querySelector(".study-previous-hours");
+const exercisePreviousHrs = document.querySelector(".exercise-previous-hours");
+const socialPreviousHrs = document.querySelector(".social-previous-hours");
+const selfCarePreviousHrs = document.querySelector(".self-previous-hours");
